1aed167024f59c579c849a08f7ec16f4bf9471d2
DeusExMortis-Eng <69949034+DeusExMortis-Eng@users.noreply.github.com>
2022-05-25 13:14:17 -0700

    Update audios-buenosdias.js

--- plugins/audios-buenosdias.js
+++ plugins/audios-buenosdias.js
@@ -6,12 +6,12 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 let vn = './media/buenos dias.mp3'
-conn.sendFile(m.chat, vn, 'buenos dias.mp3', null, m, true, {
+conn.sendFile(m.chat, vn, 'buenos dias.m4a', null, m, true, {
 type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /buenos dias|buen dia|buenos/i
+handler.customPrefix = /buenos dias/i
 handler.command = new RegExp
 handler.help = ['ʙᴜᴇɴᴏꜱ ᴅɪᴀꜱ']
 handler.tags = ['audios']
