23fc2b85db40fa1bf1a2388a7608b87be083b267
DeusExMortis-Eng <69949034+DeusExMortis-Eng@users.noreply.github.com>
2022-05-25 13:25:49 -0700

    Update audios-atencion grupo.js

--- plugins/audios-atencion grupo.js
+++ plugins/audios-atencion grupo.js
@@ -7,7 +7,7 @@ type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /atencion grupo|Atencion grupo|ATENCIÒN|AVISO IMPORTANTE|Atención grupo/i
+handler.customPrefix = /atencion pachangon|Atencion pachangon|Atención pachangon/i
 handler.command = new RegExp
 handler.help = ['ᴀᴛᴇɴᴄɪᴏɴ ɢʀᴜᴘᴏ' ]
 handler.tags = ['audios']
